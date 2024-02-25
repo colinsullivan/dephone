1c3ebbeeff7fd776db7c0554471f7c3f74b3861d
Colin Sullivan <colin@colin-sullivan.net>
2024-02-24 19:47:15 -0800

    add 100 mid show

--- interaction/interaction.ino
+++ interaction/interaction.ino
@@ -2,10 +2,13 @@
 
 Servo myservo;  // create servo object to control a servo
 int kServoOpenValue = 1000;
-int kServoCloseValue = 2150;
+int kServoCloseValue = 2250;
 
 const int buttonPin = 8;     // the number of the pushbutton pin
 const int ledPin =  10;      // the number of the LED pin
+
+// How long button must actually be pressed for
+unsigned long kButtonPressRequiredMs = 2000;
 
 // Time between pressing the button and opening the door
 unsigned long kButtonPressDelayMs = 6500;
@@ -21,8 +24,9 @@ void setup() {
     // initialize the LED pin as an output:
     // initialize the pushbutton pin as an input:
     pinMode(buttonPin, INPUT_PULLUP);
+
     // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
 
     myservo.attach(9);
 
@@ -40,33 +44,36 @@ unsigned long now = millis();
  *  Tracks the current phase
  *  0 - idle
  *  1 - button pressed, waiting
- *  2 - door opened, waiting for phone to fall
- *  3 - door closed, waiting for waiting music to finish
+ *  2 - button pressed for long enough
+ *  3 - door opened, waiting for phone to fall
+ *  4 - door closed, waiting for waiting music to finish
  **/
-int numPhases = 4;
+int numPhases = 5;
 int currentPhaseState = 0;
 unsigned long currentPhaseStartTime = 0;
 
-void handlePhaseAdvanced() {
+void handlePhaseStateChanged() {
     switch (currentPhaseState) {
         case 0:
             // idle
             break;
         case 1:
             // button pressed, waiting
+            break;
+        case 2:
 
             // Send sound computer message to move it to P1
             Serial.println("P1");  
 
             break;
-        case 2:
+        case 3:
             // door opened, waiting for phone to fall
 
             // opens servo
             myservo.writeMicroseconds(kServoOpenValue);
 
             break;
-        case 3:
+        case 4:
             // door closed, waiting for waiting music to finish
             // closes servo
             myservo.writeMicroseconds(kServoCloseValue);
@@ -77,7 +84,7 @@ void handlePhaseAdvanced() {
 void advancePhase() {
     currentPhaseState = (currentPhaseState + 1) % numPhases;
     currentPhaseStartTime = now;
-    handlePhaseAdvanced();
+    handlePhaseStateChanged();
 }
 
 void handleButtonPress() {
@@ -88,7 +95,16 @@ void handleButtonPress() {
     if (currentPhaseState == 0) {
         advancePhase();
     }
+}
+void handleButtonRelease() {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
 
+    // In phase 1, if button is released, reset the phase
+    if (currentPhaseState == 1) {
+        currentPhaseState = 0;
+        handlePhaseStateChanged();
+    }
 }
 
 unsigned long currentPhaseDuration = 0;
@@ -102,6 +118,8 @@ void loop() {
     if (prevButtonState != buttonState) {
         if (buttonState == LOW) {
             handleButtonPress();
+        } else {
+            handleButtonRelease();
         }
     }
 
@@ -111,18 +129,24 @@ void loop() {
             // idle
             break;
         case 1:
+            // button initially, waiting if it was long enough
+            if (currentPhaseDuration > kButtonPressRequiredMs) {
+                advancePhase();
+            }
+            break;
+        case 2:
             // button pressed, waiting
             if (currentPhaseDuration > kButtonPressDelayMs) {
                 advancePhase();
             }
             break;
-        case 2:
+        case 3:
             // door opened, waiting for phone to fall
             if (currentPhaseDuration > kDoorOpenDurationMs) {
                 advancePhase();
             }
             break;
-        case 3:
+        case 4:
             // door closed, waiting for waiting music to finish
             if (currentPhaseDuration > kCountdownDurationMs) {
                 advancePhase();
